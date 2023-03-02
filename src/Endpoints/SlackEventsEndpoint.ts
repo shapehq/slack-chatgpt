2cc8bbc5537a741138fb1811e223d0bc6dc93117
Simon Støvring <mail@simonbs.dk>
2023-03-02 23:53:58 +0100

    Adds SlackEventType

--- src/Endpoints/SlackEventsEndpoint.ts
+++ src/Endpoints/SlackEventsEndpoint.ts
@@ -2,6 +2,7 @@ import { Bot } from "../Bot"
 import { Endpoint } from "./Endpoint"
 import { readRequestBody } from "../readRequestBody"
 import { ResponseFactory } from "../ResponseFactory"
+import { SlackEventType } from "../Slack/SlackEventType"
 
 export class SlackEventsEndpoint implements Endpoint {
   bot: Bot
@@ -20,9 +21,9 @@ export class SlackEventsEndpoint implements Endpoint {
   
   private async handlePostRequest(request: Request): Promise<Response> {
     const body = await readRequestBody(request)
-    if (body.type == "url_verification") {
+    if (body.type == SlackEventType.URL_VERIFICATION) {
      return new Response(body.challenge)
-   } else if (body.type == "event_callback") {
+   } else if (body.type == SlackEventType.EVENT_CALLBACK) {
      await this.handleEventCallback(body.event)
      return new Response()
    } else {
@@ -34,9 +35,9 @@ export class SlackEventsEndpoint implements Endpoint {
   }
   
   private async handleEventCallback(event: any) {
-    if (event.type == "app_mention") {
+    if (event.type == SlackEventType.APP_MENTION) {
       await this.bot.postReply(event.text, event.channel, event.ts)
-    } else if (event.type == "message") {
+    } else if (event.type == SlackEventType.MESSAGE) {
       if (event.bot_profile == null) {
         // Make sure the message was not sent by a bot. If we do not have this check the bot will keep a conversation going with itself.
         await this.bot.postReply(event.text, event.channel, null)
---
+++ src/Slack/SlackEventType.ts
@@ -0,0 +1,6 @@
+export const SlackEventType = {
+  URL_VERIFICATION: "url_verification",
+  EVENT_CALLBACK: "event_callback",
+  APP_MENTION: "app_mention",
+  MESSAGE: "message",
+}
